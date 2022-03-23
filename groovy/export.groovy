7402e1c14bf28ebb04f811ef3a77112e74b2d79a
Kenan Salic <kenan.salic@bloomreach.com>
2022-03-23 10:15:06 +0100

    Revert "update"
    This reverts commit ef407369

--- groovy/export.groovy
+++ groovy/export.groovy
@@ -1,17 +1,10 @@
 package org.hippoecm.frontend.plugins.cms.admin.updater
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.commons.io.FileUtils
 import org.apache.commons.math3.random.ValueServer
 import org.onehippo.cm.ConfigurationService
 import org.onehippo.cms7.services.HippoServiceRegistry
 import org.onehippo.repository.update.BaseNodeUpdateVisitor
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
-import org.springframework.http.ResponseEntity
-import org.springframework.util.MultiValueMap
-import org.springframework.web.client.RestTemplate
 import org.yaml.snakeyaml.Yaml
 
 import javax.jcr.Node
@@ -32,6 +25,7 @@ class UpdaterEditor extends BaseNodeUpdateVisitor {
         log.debug("start")
         log.debug(configurationService.getClass().getSimpleName())
         def imp = FileUtils.getFile(FileUtils.getTempDirectory(), "import");
+        FileUtils.deleteDirectory(imp)
         imp.mkdir();
         log.debug(imp.getAbsolutePath());
         def index = FileUtils.getFile(imp.getAbsolutePath(), "_index.yaml");
@@ -45,22 +39,7 @@ class UpdaterEditor extends BaseNodeUpdateVisitor {
         Yaml yaml = new Yaml()
         LinkedHashMap<String, ArrayList> load = yaml.load(FileUtils.openInputStream(index))
         log.debug(load.toString());
-
-        ObjectMapper mapper = new ObjectMapper();
-        ValueServer restList = new ValueServer();
-        restList.setValuesFileURL("https://api.github.com/repos/ksalic/pacific-home-import/contents/")
-        List fileRegistry = mapper.readValue(restList.getValuesFileURL(), List.class);
-        log.debug("file list size = " + String.valueOf(fileRegistry.size()));
-
-        Map<String, String> fileMap = new HashMap<>();
-
-        for (LinkedHashMap file : fileRegistry) {
-            fileMap.put(file.get("name"), file.get("sha"))
-        }
-
-
-//
-//
+//        List preImport = load.get("pre-import");
         for (Map.Entry<String, ArrayList> entry : load.entrySet()) {
             def importElement = entry.getValue()
             for (LinkedHashMap<String, List<Map<String, String>>> importItem : importElement) {
@@ -86,54 +65,32 @@ class UpdaterEditor extends BaseNodeUpdateVisitor {
                             } else {
                                 FileUtils.writeStringToFile(file, configurationService.exportContent(export), "UTF-8");
                             }
-                            byte[] fileContent = FileUtils.readFileToByteArray(file);
-                            String encodedString = Base64.getEncoder().encodeToString(fileContent);
-
-                            UpdateFile updateFile = new UpdateFile();
-                            if (fileMap.containsKey(fileName)) {
-                                updateFile.setSha(fileMap.get(fileName))
-                            }
-                            updateFile.setContent(encodedString);
-                            updateFile.setMessage(new Date().toString())
-
-                            log.debug("posting.. " + fileName + " ..." + updateFile.toString())
-
-                            RestTemplate restTemplate = new RestTemplate();
-                            String fooResourceUrl = "https://api.github.com/repos/ksalic/pacific-home-import/contents/";
-
-                            HttpHeaders headers = new HttpHeaders();
-                            headers.add("Authorization", "Bearer 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");
-
-                            final HttpEntity entity = new HttpEntity(updateFile, headers);
-
-                            log.debug("running exchange..")
-
-                            ResponseEntity<String> response = restTemplate.exchange(fooResourceUrl + fileName, HttpMethod.PUT, entity, String.class);
-
-                            log.debug("result:")
-                            log.debug(response.getBody())
-                        } else {
+                        }else {
                             log.debug("node does not exist")
                         }
+
+
                     }
                 } else {
                     log.debug("node " + path + " does not exist, unable to export")
                 }
+
+
             }
         }
-//
-//
-//        def compressedExport = FileUtils.getFile(destDir, "dirCompressed.zip")
-//        def fos = FileUtils.openOutputStream(compressedExport);
-//        ZipOutputStream zipOut = new ZipOutputStream(fos);
-//        def fileToZip = destDir;
-//
-//        zipFile(fileToZip, fileToZip.getName(), zipOut);
-//        zipOut.close();
-//        fos.close();
-//
-//        def exportNode = node.addNode("export" + UUID.randomUUID().toString(), "nt:unstructured");
-//        exportNode.setProperty("zip", FileUtils.openInputStream(compressedExport));
+
+
+        def compressedExport = FileUtils.getFile(destDir, "dirCompressed.zip")
+        def fos = FileUtils.openOutputStream(compressedExport);
+        ZipOutputStream zipOut = new ZipOutputStream(fos);
+        def fileToZip = destDir;
+
+        zipFile(fileToZip, fileToZip.getName(), zipOut);
+        zipOut.close();
+        fos.close();
+
+        def exportNode = node.addNode("export" + UUID.randomUUID().toString(), "nt:unstructured");
+        exportNode.setProperty("zip", FileUtils.openInputStream(compressedExport));
 
 
     }
@@ -165,48 +122,6 @@ class UpdaterEditor extends BaseNodeUpdateVisitor {
             zipOut.write(bytes, 0, length);
         }
         fis.close();
-    }
-
-
-    public class UpdateFile {
-        private String message;
-        private String content;
-        private String sha;
-
-
-        String getMessage() {
-            return message
-        }
-
-        void setMessage(String message) {
-            this.message = message
-        }
-
-        String getContent() {
-            return content
-        }
-
-        void setContent(String content) {
-            this.content = content
-        }
-
-        String getSha() {
-            return sha
-        }
-
-        void setSha(String sha) {
-            this.sha = sha
-        }
-
-
-        @Override
-        public String toString() {
-            return "UpdateFile{" +
-                    "message='" + message + '\'' +
-                    ", content='...'" +
-                    ", sha='" + sha + '\'' +
-                    '}';
-        }
     }
 
 
